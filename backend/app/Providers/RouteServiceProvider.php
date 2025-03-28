c4863e83974063fd7dd78092e70c49fed830be66
pablovera2735 <pablo27351307@gmail.com>
2025-03-28 22:20:35 +0100

    configurando laravel

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -30,10 +30,12 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
+                ->namespace('App\Http\Controllers')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace('App\Http\Controllers')
                 ->group(base_path('routes/web.php'));
         });
     }
